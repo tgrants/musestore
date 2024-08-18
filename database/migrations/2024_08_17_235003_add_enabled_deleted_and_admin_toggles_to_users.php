38c1c29532cca34fa9b9efd7ebca336b1ed5a5f9
klappscheinwerfer <toms.grants@gmail.com>
2024-08-18 13:53:16 +0300

    Initial commit

---
+++ database/migrations/2024_08_17_235003_add_enabled_deleted_and_admin_toggles_to_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('enabled')->default(true);
+            $table->boolean('deleted')->default(false);
+            $table->boolean('admin')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('enabled');
+            $table->dropColumn('deleted');
+            $table->dropColumn('admin');
+        });
+    }
+};
